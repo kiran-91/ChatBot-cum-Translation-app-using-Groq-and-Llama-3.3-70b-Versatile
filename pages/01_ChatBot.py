2be025c2e565fe42f42972af613a893180286e5e
Kiran Deshpande <deshpandekiran007kd@gmail.com>
2024-12-18 21:03:49 +0530

    modified py files

--- pages/01_ChatBot.py
+++ pages/01_ChatBot.py
@@ -1,5 +1,4 @@
 from langchain_groq import ChatGroq
-from langchain_community.llms import Ollama
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 import streamlit as st 
--- pages/02_LanguageTranslation.py
+++ pages/02_LanguageTranslation.py
@@ -2,7 +2,6 @@ from langchain_groq import ChatGroq
 # from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
-from dotenv import load_dotenv
 import streamlit as st 
 
 groq_api_key=st.secrets["GROQ_API_KEY"]
